ae73a27d867c8f4f0a7da398c18c239f9abcf524
akbar1214 <43771058+akbar1214@users.noreply.github.com>
2019-07-19 20:11:26 +0530

    Create BUSYMAN.cpp

---
+++ BUSYMAN.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int con = static_cast<int>(1e8 + 5);
+
+int main() {
+
+    int T;
+    cin >> T;
+    while (T--) {
+
+        int no_of_activities;
+        cin >> no_of_activities;
+        vector<pair<int, int>> input(no_of_activities);
+
+        for (auto &x : input) {
+            cin >> x.first >> x.second;
+        }
+
+        sort(input.begin(), input.end(), [&](const auto &x, const auto &y) {
+
+            if (x.second != y.second) return x.second < y.second;
+            return x.first < y.first;
+        });
+
+        int curr_time = 0, cnt = 0;
+        for (int i = 0; i < no_of_activities; i++) {
+
+            if (input[i].first >= curr_time) {
+                curr_time = input[i].second;
+                ++cnt;
+            }
+        }
+        cout << cnt << '\n';
+    }
+}
