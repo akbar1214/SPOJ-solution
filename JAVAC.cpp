1678778c84147c0352efaad03c63d0eb00295957
akbar1214 <43771058+akbar1214@users.noreply.github.com>
2019-07-20 14:00:15 +0530

    Create JAVAC.cpp

---
+++ JAVAC.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int con = static_cast<int>(1e8 + 5);
+
+void to_cpp(string &s) {
+
+    string ans;
+    for (auto &x:s) {
+
+        if (isupper(x)) {
+            ans += "_";
+        }
+        ans += (char) tolower(x);
+    }
+    cout << ans << '\n';
+}
+
+void toJava(string &s) {
+
+    bool flag = false;
+    string ans;
+    for (auto &x:s) {
+
+        if (x == '_' && !flag) {
+            flag = true;
+            continue;
+        }
+        if (flag && x == '_') {
+            cout << "Error!\n";
+            return;
+        }
+        if (flag) {
+
+            flag = false;
+            ans += (char) toupper(x);
+        } else {
+            ans += (char) x;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+
+    string s;
+    while (cin >> s) {
+        bool is_cpp = false, isJava = false;
+        for (char &x:s) {
+
+            if (x == '_') {
+                is_cpp = true;
+            }
+            if (isupper(x)) {
+                isJava = true;
+            }
+        }
+        if ((is_cpp && isJava) || isupper(s[0]) || s[0] == '_' || s[s.size() - 1] == '_') {
+            cout << "Error!";
+        } else {
+
+            is_cpp ? toJava(s) : to_cpp(s);
+        }
+    }
+}
