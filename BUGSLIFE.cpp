0d608f16bf88dc5337df20d3d6f4d5a079fcb501
akbar1214 <43771058+akbar1214@users.noreply.github.com>
2019-07-20 14:03:42 +0530

    Update BUGSLIFE.cpp

--- BUGSLIFE.cpp
+++ BUGSLIFE.cpp
@@ -1,30 +1,24 @@
 #include<bits/stdc++.h>
 
-
-//#define _debug
-
 using namespace std;
 
-
-
-bool isBipartite(vector<int> v[2005],int src,int color[2005]){
-
+bool isBipartite(vector<int> v[2005], int src, int color[2005]) {
 
     queue<int> qe;
     qe.push(src);
     color[src] = 1;
 
-    while(!qe.empty()){
+    while (!qe.empty()) {
 
-        int u = qe.front();qe.pop();
+        int u = qe.front();
+        qe.pop();
 
-        for(int i=0;i<v[u].size();i++){
+        for (int i = 0; i < v[u].size(); i++) {
 
-            if(color[v[u][i]] == color[u] || v[u][i] == u){
+            if (color[v[u][i]] == color[u] || v[u][i] == u) {
                 return false;
-            }
-            else if(color[v[u][i]] == -1){
-                color[v[u][i]] = 1-color[u];
+            } else if (color[v[u][i]] == -1) {
+                color[v[u][i]] = 1 - color[u];
                 qe.push(v[u][i]);
             }
         }
@@ -34,47 +28,39 @@ bool isBipartite(vector<int> v[2005],int src,int color[2005]){
     return true;
 }
 
+int main() {
 
+    int T;
+    cin >> T;
+    for (int k = 1; k <= T; k++) {
 
-int main(){
-
-    int T;cin >> T;
-    for(int k=1;k<=T;k++){
-
-        int no_bugs,no_interaction;
+        int no_bugs, no_interaction;
         cin >> no_bugs >> no_interaction;
 
         vector<int> v[2005];
 
-        for(int i=0;i<no_interaction;i++){
-            int a,b;cin >> a >> b;
+        for (int i = 0; i < no_interaction; i++) {
+            int a, b;
+            cin >> a >> b;
             v[a].push_back(b);
             v[b].push_back(a);
         }
 
-
         int color[2005];
-        for(int i=0;i<2005;i++)
-            color[i] = -1;
-
+        for (int &i : color)
+            i = -1;
 
         bool answer = true;
 
-        for(int i=1;i<=no_bugs;i++){
+        for (int i = 1; i <= no_bugs; i++) {
 
-            if(color[i] == -1 && v[i].size() && answer){
-                answer = isBipartite(v,i,color);
+            if (color[i] == -1 && !v[i].empty() && answer) {
+                answer = isBipartite(v, i, color);
             }
         }
 
-
-        cout << "Scenario #" <<k << ":\n";
-        if(answer == false){
-
-            cout << "Suspicious bugs found!";
-        }else{
-            cout << "No suspicious bugs found!";
-        }
+        cout << "Scenario #" << k << ":\n";
+        cout << (!answer ? "Suspicious bugs found!" : "No suspicious bugs found!");
         cout << endl;
     }
 }
