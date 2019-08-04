3acaeb983419dc5bf79c24f04b896614481b6c10
akbar1214 <43771058+akbar1214@users.noreply.github.com>
2019-08-04 18:03:24 +0530

    Create DANGER.java

---
+++ DANGER.java
@@ -0,0 +1,149 @@
+import java.io.*;
+
+class Main {
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[1024]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public String read_s() throws IOException {
+            byte[] buf = new byte[1024]; // line length
+            int cnt = 0, c;
+
+            while ((c = read()) != -1) {
+                if (c == ' ' || c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        Reader cin = new Reader();
+        PrintWriter pw = new PrintWriter(System.out);
+
+        ////////////////////////////////////////////////////////////////////////////
+
+        char[] arr = new char[5];
+        arr = cin.readLine().toCharArray();
+        int number = ((arr[0] - '0') * 10 + (arr[1] - '0')) * (int) Math.pow((double) 10, (double) (arr[3] - '0'));
+        while (number > 0) {
+
+            int p = 1;
+            while (p <= number) p *= 2;
+            pw.println(2 * number - p + 1);
+            arr = cin.readLine().toCharArray();
+            number = ((arr[0] - '0') * 10 + (arr[1] - '0')) * (int) Math.pow((double) 10, (double) (arr[3] - '0'));
+        }
+
+        //////////////////////////////////////////////////////////////////////////////
+        pw.flush();
+        pw.close();
+    }
+
+}
